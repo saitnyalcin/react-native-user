7aac0742750d7889329bd2c89de7b5743a75adda
Steve Yalcin <steveyalcin@Steves-MacBook-Pro.local>
2022-02-23 14:11:01 -0500

    random user api call is implemented

--- App.js
+++ App.js
@@ -1,11 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { FlatList, Image, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  console.log(data);
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api/")
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {isLoading ? (
+        <Text>Loading...</Text>
+      ) : (
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+        >
+          <Text style={{ fontSize: 18, color: "green", textAlign: "center" }}>
+            Random User Info Generator
+          </Text>
+          <FlatList
+            data={data.results}
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item }) => (
+              <View style={styles.container}>
+                <Image
+                  style={styles.userImage}
+                  source={{
+                    uri: item.picture.thumbnail,
+                  }}
+                />
+                <Text>{item.gender}</Text>
+                <Text>
+                  {item.name.title +
+                    ". " +
+                    item.name.first +
+                    " " +
+                    item.name.last}
+                </Text>
+                <Text>{item.location.city}</Text>
+                <Text>{item.email}</Text>
+                <Text>{item.phone}</Text>
+              </View>
+            )}
+          />
+        </View>
+      )}
     </View>
   );
 }
@@ -13,8 +63,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  userImage: {
+    margin: 25,
+    width: 100,
+    height: 100,
+    borderRadius: 100,
   },
 });
